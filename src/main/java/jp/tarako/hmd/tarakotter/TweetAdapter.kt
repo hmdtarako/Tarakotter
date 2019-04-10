aeba2fc18820d8c21a05c90533fdfa8ec29ed8eb
hmdtarako <hmd_tarako@yahoo.co.jp>
2019-04-10 23:59:48 +0900

    ViewHolderパターンを適用。

--- src/main/java/jp/tarako/hmd/tarakotter/TweetAdapter.kt
+++ src/main/java/jp/tarako/hmd/tarakotter/TweetAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.twitter.sdk.android.core.models.Tweet
 import kotlinx.android.synthetic.main.row_tweet.view.*
 import java.net.URL
@@ -32,24 +34,37 @@ class TweetAdapter(context: Context, val tweetList: List<Tweet>) : BaseAdapter()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: inflater.inflate(R.layout.row_tweet, parent, false)
+        val (viewHolder, view) = if(convertView == null) {
+            val tweetView = inflater.inflate(R.layout.row_tweet, parent, false)
+            val screenName = tweetView.screen_name
+            val tweetBody = tweetView.tweet_body
+            val recentMark = tweetView.recent_mark
+            val icon = tweetView.icon
+            val viewHolder = ViewHolder(screenName, tweetBody, recentMark, icon)
+            tweetView.tag = viewHolder
+            Pair(viewHolder, tweetView)
+        } else {
+            Pair(convertView.tag as ViewHolder, convertView)
+        }
 
-        view.screen_name.text = "${tweetList[position].user.name}@${tweetList[position].user.screenName}"
-        view.tweet_body.text = tweetList[position].text
+        viewHolder.screenName.text = "${tweetList[position].user.name}@${tweetList[position].user.screenName}"
+        viewHolder.tweetBody.text = tweetList[position].text
 
         val dateTweet = dateFormat.parse(tweetList[position].createdAt)
         val dateCurrentLimit = Date(System.currentTimeMillis() - CURRENT_LIMIT_DELAY)
+        val isRecent = dateTweet.after(dateCurrentLimit)
+        viewHolder.recentMark.visibility = if (isRecent) View.VISIBLE else View.GONE
 
-        if (dateTweet.after(dateCurrentLimit)) {
-            view.recent_mark.visibility = View.VISIBLE
-        } else {
-            view.recent_mark.visibility = View.GONE
-        }
-
-        val task = ImageDownloadTask(view.icon)
+        val task = ImageDownloadTask(viewHolder.icon)
         val url = URL(tweetList[position].user.profileImageUrl)
         task.execute(url)
 
         return view
     }
+
+    class ViewHolder(
+        val screenName: TextView,
+        val tweetBody: TextView,
+        val recentMark: TextView,
+        val icon: ImageView)
 }
